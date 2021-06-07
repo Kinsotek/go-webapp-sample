f1aa63be3447ec5ca91298f2f8cc03377fd88b99
tactical <1tacticalretard@github.com>
2021-06-07 15:18:54 +0300

    Added bells and whistles

--- docker_run.sh
+++ docker_run.sh
@@ -3,8 +3,10 @@ echo "Should I run go-webapp-sample (Y/n)?"
 read answer
 if [[ $answer  == "y"  || $answer == "Y" ]];
 then
-echo "Enjoy it."
-echo `docker run -it -p 8080:8080 --rm go-webapp-sample sh -c 'cd go-webapp-sample && ./go-webapp-sample'`
+echo "Okay, what is the port to be used for the purpose?"
+read port
+echo "Enjoy it from now on through the link of: http://localhost:$port "
+echo `docker run -it -p $port:8080 --rm go-webapp-sample sh -c 'cd go-webapp-sample && ./go-webapp-sample'`
 else
 
 echo "Okay. Exiting."
